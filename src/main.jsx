8b5c7395b9aa480165a9d95d9550287a9a4c36f9
pierslabs <pedrolosasp@gmail.com>
2023-07-26 19:43:53 +0200

    add reactQuery

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/app.router';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
