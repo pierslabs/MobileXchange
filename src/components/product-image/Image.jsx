9d26a658b33fcacff1b0f86a58436f43030053f2
pierslabs <pedrolosasp@gmail.com>
2023-07-28 21:48:38 +0200

    create image-component

--- src/components/product-image/Image.jsx
+++ src/components/product-image/Image.jsx
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 
 const Image = ({ url, alt }) => {
-  console.log(url, alt);
-
   return <img src={url} alt={alt} className='w-full h-auto' />;
 };
 
 export default Image;
 
 Image.propTypes = {
-  url: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
+  url: PropTypes.string,
+  alt: PropTypes.string,
 };
