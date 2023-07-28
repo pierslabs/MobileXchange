ee8af3746050c50fe9ebccd9dfb62cc90309b26e
pierslabs <pedrolosasp@gmail.com>
2023-07-28 07:26:54 +0200

    Add Link to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
 import ShopppingBag from '../shopping-bag/ShopppingBag';
 
 const Header = () => {
   return (
     <header className='flex justify-between  bg-gradient-to-r from-blue-500 to-purple-400 p-2 text-white text-center'>
-      <h1 className='text-2xl  m-1'>MobileXchange</h1>
+      <h1 className='text-2xl  m-1'>
+        <Link to='/'>MobileXchange</Link>
+      </h1>
       <ShopppingBag />
     </header>
   );
