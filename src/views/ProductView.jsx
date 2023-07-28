0ed33954ec38f971a73f5975266c5e2dd5bee242
pierslabs <pedrolosasp@gmail.com>
2023-07-28 07:29:53 +0200

    Add in product-view --> imageComponent && productDetail

--- src/views/ProductView.jsx
+++ src/views/ProductView.jsx
@@ -1,10 +1,37 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
+import Loader from '../components/loader/Loader';
+import Image from '../components/product-image/Image';
+import ProductDetail from '../components/product-detail/ProductDetail';
 
 const ProductView = () => {
-  const navigate = useNavigate();
+  const params = useParams();
+
+  const {
+    products = [],
+    isLoading,
+    error,
+  } = useFetch({
+    url: `/product/${params.id}`,
+    label: `product-detail-${params.id}`,
+    stale: true,
+  });
+
+  console.log(products, isLoading, error);
+
   return (
-    <div>
-      <button onClick={() => navigate('/products/list')}>Click</button>
+    <div className='container mx-auto px-4 py-8 '>
+      {isLoading && <Loader />}
+      <Link to='/products/list' className='text-blue-500 hover:text-blue-600'>
+        Back to Products
+      </Link>
+      <div className='flex justify-evenly  '>
+        <div className='sm:w-96'>
+          <Image url={products.imgUrl} alt={products.name} />
+        </div>
+
+        <ProductDetail product={products} />
+      </div>
     </div>
   );
 };
