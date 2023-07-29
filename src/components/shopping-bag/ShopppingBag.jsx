9a8177c0ba241bf48974b4921d881387991f768e
pierslabs <pedrolosasp@gmail.com>
2023-07-29 11:07:45 +0200

    add cartProducts from context in shopping-bag

--- src/components/shopping-bag/ShopppingBag.jsx
+++ src/components/shopping-bag/ShopppingBag.jsx
@@ -1,14 +1,16 @@
+import { useContext } from 'react';
 import { BiShoppingBag } from 'react-icons/bi';
+import { ProductAppContext } from '../../context/ProductAppContext';
 
 const ShopppingBag = () => {
-  const itemCount = 10;
+  const { cartProducts } = useContext(ProductAppContext);
 
   return (
     <div className='relative m-2'>
       <BiShoppingBag className='text-2xl' />
-      {itemCount > 0 && (
+      {cartProducts > 0 && (
         <div className='w-5 h-5 bg-red-500 text-white rounded-full flex items-center justify-center text-xs absolute -top-2 -right-2'>
-          {itemCount}
+          {cartProducts}
         </div>
       )}
     </div>
